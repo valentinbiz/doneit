7bef4cb4236ad786fc1a8d89924c7000e6c23509
Valentin Bizgan <valentin.bizgan@outlook.com>
2023-02-08 11:23:50 +0000

    Custom proxy

--- Main/Quote/quote.js
+++ Main/Quote/quote.js
@@ -17,12 +17,12 @@ function complete() {
 
 async function getQuote() {
   loading();
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const proxyUrl = "https://obscure-wave-23702.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
     console.log(data);
     authorText.innerText = data.author;
