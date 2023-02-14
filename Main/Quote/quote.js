c02c1151d3d53695c6c2a66c2d0664bf3b53785f
Valentin Bizgan <valentin.bizgan@outlook.com>
2023-02-14 16:29:45 +0000

    Fix Quote

--- Main/Quote/quote.js
+++ Main/Quote/quote.js
@@ -22,7 +22,7 @@ async function getQuote() {
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
   try {
-    const response = await fetch(proxyUrl + apiUrl);
+    const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
     authorText.innerText = data.quoteAuthor;
